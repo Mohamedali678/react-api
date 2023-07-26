5d6706717787f6c1ad7d5e21d26f75615e9d1715
mohamedali678 <mohamedalidahir678@gmail.com>
2023-07-26 12:04:31 +0300

    This is test of using API

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+
+import axios from "axios";
+import {useState} from "react";
+
+function App() {
+
+
+  const [photos, setPhotos] = useState([]);
+
+
+  const getAllPhotos = () => {
+    axios.get("https://jsonplaceholder.typicode.com/photos").then((response) => {
+     
+    setPhotos(response.data)
+    })
+  }
+
+
+  return <div>
+
+      <button onClick={getAllPhotos}>Get Data</button>
+
+        {
+          
+          photos.map((photo, index) => (
+            <div className="item-parent">
+            <div className="items">
+            
+
+            <p> {photo.title} </p>
+            <img src={photo.thumbnailUrl} />
+              
+
+              </div>
+            </div>
+          ))
+
+         
+        }
+
+  </div>
+
+
+}
+export default App;
